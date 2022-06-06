4c8b6186ebdbae50ee3c1ca8441a0a499cfd7204
pvishnu1 <93652983+pvishnu1@users.noreply.github.com>
2022-06-06 17:51:20 +0530

    profile update

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,6 +6,7 @@ import { LoginComponent } from './login/login.component';
 import { HomeComponent } from './home/home.component';
 import { PostComponent } from './post/post.component';
 import { TimelineComponent } from './timeline/timeline.component';
+import { ProfileComponent } from './profile/profile.component';
 
 const routes: Routes = [
   { path: 'home', component: HomeComponent },
@@ -13,6 +14,7 @@ const routes: Routes = [
   { path: 'register', component: RegisterComponent },
   { path: 'createPost', component: PostComponent },
   { path: 'timeline', component: TimelineComponent },
+  { path: 'profile', component: ProfileComponent },
   { path: '', redirectTo: 'home', pathMatch: 'full' }
 ];
 
--- src/app/profile/profile.component.ts
+++ src/app/profile/profile.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { TokenStorageService } from '../_services/token-storage.service';
 
 @Component({
   selector: 'app-profile',
@@ -6,10 +7,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./profile.component.css']
 })
 export class ProfileComponent implements OnInit {
+ 
+  userName: String = "";
 
-  constructor() { }
+  constructor(
+    private tokenStorage: TokenStorageService,
+  ) {
+    
+   }
 
   ngOnInit(): void {
+     this.userName = this.tokenStorage.getUser();
   }
 
 }
